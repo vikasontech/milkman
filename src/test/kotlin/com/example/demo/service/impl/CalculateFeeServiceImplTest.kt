9408141cde1816f3528f3541601ffa999442a9c3
vikas <vikas.on@gmail.com>
2019-08-16 19:49:06 +0700

    work in progress

--- src/test/kotlin/com/example/demo/service/impl/CalculateFeeServiceImplTest.kt
+++ src/test/kotlin/com/example/demo/service/impl/CalculateFeeServiceImplTest.kt
@@ -2,6 +2,8 @@ package com.example.demo.service.impl
 
 import com.example.demo.service.WeekDays
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.apache.coyote.http11.Constants.a
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -10,20 +12,15 @@ import org.reactivestreams.Publisher
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.core.publisher.toFlux
+import reactor.test.StepVerifier
+import java.io.File
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.function.BiFunction
+import java.util.stream.Collectors
 
 @RunWith(MockitoJUnitRunner::class)
 class CalculateFeeServiceImplTest {
-
-  @Test
-  fun getTotalNoOFDays() {
-    val ld = LocalDate.of(2019, 5, 1)
-    println(ld.lengthOfMonth())
-//    calculateInvoiceDetails(noOfDays = ld.lengthOfMonth())
-  }
-
 
   @Test
   fun randomTest() {
@@ -41,32 +38,20 @@ class CalculateFeeServiceImplTest {
 //        descriptions = invoiceDetails, month = 0, year = 0)
 //    println(invoice)
 
-    calculateInvoiceDetails(mapConfig= mapConfig,
-        price = BigDecimal.valueOf(37),
-        month = 7,
-        year = 2019)
-        .map { e -> {
-          println(e)
-          val om = ObjectMapper()
-          om.writeValueAsString(e)
-        } }
-        .subscribe { e-> println(e)}
+
+    calculateInvoiceDetails(mapConfig = mapConfig, price = BigDecimal.valueOf(37), month = 7, year = 2019).collect(Collectors.toList()).subscribe { e -> println(ObjectMapper().writeValueAsString(e)) }
   }
 
   @Test
   fun testGetTotalCost() {
-
+    getTotalCost(Flux.fromIterable(dummyInvoiceDetails())).subscribe { e -> println(e) }
   }
+
   private fun getTotalCost(invoiceDetail: Flux<InvoiceDetail>): Mono<BigDecimal> {
-    return invoiceDetail
-        .map { e -> e.totalCostPerDay }
-        .reduce{ a, b -> a.add(b)}
+    return invoiceDetail.map { e -> e.totalCostPerDay }.reduce { a, b -> a.add(b) }
   }
 
-  private fun calculateInvoiceDetails(mapConfig:Map<Int, Long>,
-                                      price: BigDecimal,
-                                      year: Int,
-                                      month: Int): Flux<InvoiceDetail> {
+  private fun calculateInvoiceDetails(mapConfig: Map<Int, Long>, price: BigDecimal, year: Int, month: Int): Flux<InvoiceDetail> {
 
     val localDate = LocalDate.of(year, month, 1)
     val totalDays = localDate.lengthOfMonth()
@@ -80,27 +65,19 @@ class CalculateFeeServiceImplTest {
       val pricePerDay = price.multiply(BigDecimal.valueOf(milkPerDay!!))
       totalPrice += pricePerDay
 
-      invoiceDetails.add(InvoiceDetail(date = i, dayInWord = WeekDays.new(tempStartDay),
-          milkPerDay = milkPerDay.toInt(), totalCostPerDay = pricePerDay))
+      invoiceDetails.add(InvoiceDetail(date = i, dayInWord = WeekDays.new(tempStartDay), milkPerDay = milkPerDay.toInt(), totalCostPerDay = pricePerDay))
 
       tempStartDay = if ((tempStartDay + 1) > 6) 0 else tempStartDay + 1
     }
     return Flux.fromIterable(invoiceDetails)
   }
+
+  fun dummyInvoiceDetails(): List<InvoiceDetail> {
+    return ObjectMapper().registerModule(KotlinModule()).readValue<List<InvoiceDetail>>(File("src/test/resources/InvoiceDetails.json"))
+
+  }
 }
 
-data class InvoiceDetail(
-    val date: Int,
-    val dayInWord: WeekDays,
-    val milkPerDay: Int,
-    val totalCostPerDay: BigDecimal
-)
+data class InvoiceDetail(val date: Int, val dayInWord: WeekDays, val milkPerDay: Int, val totalCostPerDay: BigDecimal)
 
-data class Invoice(
-    val name: String,
-    val month: Int,
-    val year: Int,
-    val vendorName: String,
-    val billingDate: LocalDate,
-    val descriptions: List<InvoiceDetail>
-)
+data class Invoice(val name: String, val month: Int, val year: Int, val vendorName: String, val billingDate: LocalDate, val descriptions: List<InvoiceDetail>)
